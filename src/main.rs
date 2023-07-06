bf92beb6ace170f86bc3c36065623f8e28b901b3
Arjun31415 <arjunp0710@gmail.com>
2023-07-06 21:08:49 +0530

    boids but not worky

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod graphics;
 use crate::graphics::framework::Wallpaper;
+use nanorand::{Rng, WyRand};
 use smithay_client_toolkit::{
     compositor::CompositorHandler,
     output::{OutputHandler, OutputState},
@@ -11,19 +12,39 @@ use smithay_client_toolkit::{
         Capability, SeatHandler, SeatState,
     },
     shell::{
-        wlr_layer::{
-            LayerShellHandler, LayerSurface,
-            LayerSurfaceConfigure,
-        },
+        wlr_layer::{LayerShellHandler, LayerSurface, LayerSurfaceConfigure},
         WaylandSurface,
     },
 };
 use std::borrow::Cow;
+use std::sync::Mutex;
 use wayland_client::{
     protocol::{wl_keyboard, wl_output, wl_pointer, wl_seat, wl_surface},
     Connection, QueueHandle,
 };
-
+use wgpu::util::DeviceExt;
+struct Boid {
+    num_particles: u32,
+    particles_per_group: u32,
+    frame_num: usize,
+    compute_pipeline: Option<wgpu::ComputePipeline>,
+    render_pipeline: Option<wgpu::RenderPipeline>,
+    particle_bind_groups: Vec<wgpu::BindGroup>,
+    work_group_count: u32,
+    particle_buffers: Vec<wgpu::Buffer>,
+    vertices_buffer: Option<wgpu::Buffer>,
+}
+static BOID: Mutex<Boid> = Mutex::new(Boid {
+    particle_buffers: vec![],
+    work_group_count: 0,
+    num_particles: 1500,
+    particles_per_group: 64,
+    frame_num: 0,
+    compute_pipeline: None,
+    render_pipeline: None,
+    particle_bind_groups: vec![],
+    vertices_buffer: None,
+});
 use xkbcommon::xkb::keysyms;
 impl CompositorHandler for Wallpaper {
     fn scale_factor_changed(
@@ -44,7 +65,7 @@ impl CompositorHandler for Wallpaper {
         _time: u32,
     ) {
         // println!("frame");
-        self.draw(qh);
+        self.render(qh);
     }
 }
 
@@ -102,7 +123,7 @@ impl LayerShellHandler for Wallpaper {
         // Initiate the first draw.
         if self.first_configure {
             self.first_configure = false;
-            self.draw(qh);
+            self.init(qh);
         }
     }
 }
@@ -241,7 +262,8 @@ impl PointerHandler for Wallpaper {
         use PointerEventKind::*;
         for event in events {
             // Ignore events for other surfaces
-            if &event.surface != self.layer.wl_surface() { continue;
+            if &event.surface != self.layer.wl_surface() {
+                continue;
             }
             println!("event");
             match event.kind {
@@ -277,37 +299,138 @@ impl PointerHandler for Wallpaper {
 }
 
 impl Wallpaper {
-    pub fn draw(&mut self, _qh: &QueueHandle<Self>) {
+    pub fn init(&mut self, _qh: &QueueHandle<Self>) {
         let adapter = &self.adapter;
         let surface = &self.surface;
         let device = &self.device;
         let queue = &self.queue;
+        let mut boid_guard = BOID.lock().unwrap();
 
-        let swapchain_capabilities = surface.get_capabilities(&adapter);
-        let swapchain_format = swapchain_capabilities.formats[0];
+        /* let swapchain_capabilities = surface.get_capabilities(&adapter);
+        let swapchain_format = swapchain_capabilities.formats[0]; */
         // Load the shaders from disk
-        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        let compute_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: None,
-            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("compute.wgsl"))),
         });
-        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        let draw_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: None,
-            bind_group_layouts: &[],
-            push_constant_ranges: &[],
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("draw.wgsl"))),
         });
+        let sim_param_data = [
+            0.04f32, // deltaT
+            0.1,     // rule1Distance
+            0.025,   // rule2Distance
+            0.025,   // rule3Distance
+            0.02,    // rule1Scale
+            0.05,    // rule2Scale
+            0.005,   // rule3Scale
+        ]
+        .to_vec();
+        let sim_param_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Simulation Parameter Buffer"),
+            contents: bytemuck::cast_slice(&sim_param_data),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+        let default_config = surface
+            .get_default_config(&adapter, self.width, self.height)
+            .expect("Surface isn't supported by the adapter.");
+        let mut surface_config = wgpu::SurfaceConfiguration {
+            /* format: swapchain_format,
+            view_formats: vec![swapchain_format], */
+            // alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            width: self.width,
+            height: self.height,
+            // Wayland is inherently a mailbox system.
+            present_mode: wgpu::PresentMode::Mailbox,
+            ..default_config
+        };
+        let surface_view_format = surface_config.format.add_srgb_suffix();
+        surface_config.view_formats.push(surface_view_format);
+        // create compute bind layout group and compute pipeline layout
+        surface.configure(&self.device, &surface_config);
+
+        let compute_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: wgpu::BufferSize::new(
+                                (sim_param_data.len() * std::mem::size_of::<f32>()) as _,
+                            ),
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
+                            has_dynamic_offset: false,
+                            min_binding_size: wgpu::BufferSize::new(
+                                (boid_guard.num_particles * 16) as _,
+                            ),
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 2,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: wgpu::BufferSize::new(
+                                (boid_guard.num_particles * 16) as _,
+                            ),
+                        },
+                        count: None,
+                    },
+                ],
+                label: None,
+            });
+        let compute_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("compute"),
+                bind_group_layouts: &[&compute_bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        // create render pipeline with empty bind group layout
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("render"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
 
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: None,
-            layout: Some(&pipeline_layout),
+            layout: Some(&render_pipeline_layout),
             vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main",
-                buffers: &[],
+                module: &draw_shader,
+                entry_point: "main_vs",
+                buffers: &[
+                    wgpu::VertexBufferLayout {
+                        array_stride: 4 * 4,
+                        step_mode: wgpu::VertexStepMode::Instance,
+                        attributes: &wgpu::vertex_attr_array![0 => Float32x2, 1 => Float32x2],
+                    },
+                    wgpu::VertexBufferLayout {
+                        array_stride: 2 * 4,
+                        step_mode: wgpu::VertexStepMode::Vertex,
+                        attributes: &wgpu::vertex_attr_array![2 => Float32x2],
+                    },
+                ],
             },
             fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[Some(swapchain_format.into())],
+                module: &draw_shader,
+                entry_point: "main_fs",
+                targets: &[Some(surface_config.view_formats[0].into())],
             }),
             primitive: wgpu::PrimitiveState::default(),
             depth_stencil: None,
@@ -315,51 +438,175 @@ impl Wallpaper {
             multiview: None,
         });
 
-        let surface_config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: swapchain_format,
-            view_formats: vec![swapchain_format],
-            alpha_mode: wgpu::CompositeAlphaMode::Auto,
-            width: self.width,
-            height: self.height,
-            // Wayland is inherently a mailbox system.
-            present_mode: wgpu::PresentMode::Mailbox,
-        };
+        // create compute pipeline
 
-        surface.configure(&self.device, &surface_config);
+        let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: Some("Compute pipeline"),
+            layout: Some(&compute_pipeline_layout),
+            module: &compute_shader,
+            entry_point: "main",
+        });
 
-        // We don't plan to render much in this example, just clear the surface.
-        let surface_texture = surface
-            .get_current_texture()
-            .expect("failed to acquire next swapchain texture");
-        let texture_view = surface_texture
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
+        // buffer for the three 2d triangle vertices of each instance
 
-        let mut encoder = device.create_command_encoder(&Default::default());
-        {
-            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: None,
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &texture_view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-            });
-            rpass.set_pipeline(&render_pipeline);
-            rpass.draw(0..3, 0..1);
+        let vertex_buffer_data = [-0.01f32, -0.02, 0.01, -0.02, 0.00, 0.02];
+        let vertices_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::bytes_of(&vertex_buffer_data),
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+        });
+
+        // buffer for all particles data of type [(posx,posy,velx,vely),...]
+
+        let mut initial_particle_data = vec![0.0f32; (4 * boid_guard.num_particles) as usize];
+        let mut rng = WyRand::new_seed(42);
+        let mut unif = || rng.generate::<f32>() * 2f32 - 1f32; // Generate a num (-1, 1)
+        for particle_instance_chunk in initial_particle_data.chunks_mut(4) {
+            particle_instance_chunk[0] = unif(); // posx
+            particle_instance_chunk[1] = unif(); // posy
+            particle_instance_chunk[2] = unif() * 0.1; // velx
+            particle_instance_chunk[3] = unif() * 0.1; // vely
         }
 
-        // Submit the command in the queue to execute
-        queue.submit(Some(encoder.finish()));
+        // creates two buffers of particle data each of size NUM_PARTICLES
+        // the two buffers alternate as dst and src for each frame
+
+        let mut particle_buffers = Vec::<wgpu::Buffer>::new();
+        let mut particle_bind_groups = Vec::<wgpu::BindGroup>::new();
+        for i in 0..2 {
+            particle_buffers.push(
+                device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some(&format!("Particle Buffer {i}")),
+                    contents: bytemuck::cast_slice(&initial_particle_data),
+                    usage: wgpu::BufferUsages::VERTEX
+                        | wgpu::BufferUsages::STORAGE
+                        | wgpu::BufferUsages::COPY_DST,
+                }),
+            );
+        }
+
+        // create two bind groups, one for each buffer as the src
+        // where the alternate buffer is used as the dst
+
+        for i in 0..2 {
+            particle_bind_groups.push(device.create_bind_group(&wgpu::BindGroupDescriptor {
+                layout: &compute_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: sim_param_buffer.as_entire_binding(),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: particle_buffers[i].as_entire_binding(),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 2,
+                        resource: particle_buffers[(i + 1) % 2].as_entire_binding(), // bind to opposite buffer
+                    },
+                ],
+                label: None,
+            }));
+        }
+
+        // calculates number of work groups from PARTICLES_PER_GROUP constant
+        let work_group_count = ((boid_guard.num_particles as f32)
+            / (boid_guard.particles_per_group as f32))
+            .ceil() as u32;
+
+        *boid_guard = Boid {
+            particle_buffers,
+            particle_bind_groups,
+            work_group_count,
+            frame_num: 0,
+            render_pipeline: Some(render_pipeline),
+            compute_pipeline: Some(compute_pipeline),
+            vertices_buffer: Some(vertices_buffer),
+            ..*boid_guard
+        };
         self.wl_surface
             .damage_buffer(0, 0, self.width as i32, self.height as i32);
         self.wl_surface.frame(_qh, self.wl_surface.clone());
-        surface_texture.present();
+        self.layer.commit();
+        self.wl_surface.commit();
+    }
+    fn render(&mut self, _qh: &QueueHandle<Self>) {
+        let mut boid_guard = BOID.lock().unwrap();
+        let adapter = &self.adapter;
+        let surface = &self.surface;
+        let device = &self.device;
+        let queue = &self.queue;
+        let surface_texture = surface
+            .get_current_texture()
+            .expect("failed to acquire next swapchain texture");
+
+        let view = &surface_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let color_attachments = [Some(wgpu::RenderPassColorAttachment {
+            view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+                // Not clearing here in order to test wgpu's zero texture initialization on a surface texture.
+                // Users should avoid loading uninitialized memory since this can cause additional overhead.
+                load: wgpu::LoadOp::Load,
+                store: true,
+            },
+        })];
+        let render_pass_descriptor = wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &color_attachments,
+            depth_stencil_attachment: None,
+        };
+
+        // get command encoder
+        let mut command_encoder =
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        command_encoder.push_debug_group("compute boid movement");
+        {
+            // compute pass
+            let mut cpass =
+                command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
+            cpass.set_pipeline(&(*boid_guard).compute_pipeline.as_ref().unwrap());
+            cpass.set_bind_group(
+                0,
+                &(*boid_guard).particle_bind_groups[boid_guard.frame_num % 2],
+                &[],
+            );
+            cpass.dispatch_workgroups((*boid_guard).work_group_count, 1, 1);
+        }
+        command_encoder.pop_debug_group();
+
+        command_encoder.push_debug_group("render boids");
+        {
+            // render pass
+            let mut rpass = command_encoder.begin_render_pass(&render_pass_descriptor);
+            rpass.set_pipeline(&boid_guard.render_pipeline.as_ref().unwrap());
+            // render dst particles
+            rpass.set_vertex_buffer(
+                0,
+                <Vec<wgpu::Buffer> as AsRef<Vec<wgpu::Buffer>>>::as_ref(
+                    &boid_guard.particle_buffers,
+                )[(boid_guard.frame_num + 1) % 2]
+                    .slice(..),
+            );
+            // the three instance-local vertices
+            rpass.set_vertex_buffer(1, boid_guard.vertices_buffer.as_ref().unwrap().slice(..));
+            rpass.draw(0..3, 0..boid_guard.num_particles);
+        }
+        command_encoder.pop_debug_group();
+
+        // update frame count
+        (*boid_guard).frame_num += 1;
+
+        // done
+        queue.submit(Some(command_encoder.finish()));
+        self.wl_surface
+            .damage_buffer(0, 0, self.width as i32, self.height as i32);
+        self.wl_surface.frame(_qh, self.wl_surface.clone());
+        // surface_texture.present();
         self.layer.commit();
         self.wl_surface.commit();
     }
