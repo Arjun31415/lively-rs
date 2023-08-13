cf50a0da0c550595ec1f04d4ac25fb0faf90efb2
Arjun31415 <arjunp0710@gmail.com>
2023-08-13 18:42:59 +0530

    added nix flake

--- flake.nix
+++ flake.nix
@@ -4,13 +4,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    nixgl.url = "github:guibou/nixGL";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs:
+  outputs = {
+    nixgl,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [nixgl.overlay];
+    };
+  in
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
 
@@ -30,6 +40,7 @@
             rustc
             rustfmt
             rustPackages.clippy
+            glxinfo
           ];
 
           RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
@@ -38,7 +49,7 @@
         packages =
           {
             lively = pkgs.rustPlatform.buildRustPackage {
-              pname = "lively";
+              pname = "wgpu";
               version = "0.1.0";
 
               src = ./.;
@@ -49,11 +60,19 @@
 
               nativeBuildInputs = with pkgs; [pkg-config];
               buildInputs = with pkgs; [
+                glxinfo
                 libinput
                 libxkbcommon
                 udev
                 wayland
+                vulkan-loader
+                vulkan-validation-layers
+                vulkan-headers
+                vulkan-tools
+                mesa.drivers
+                libinput
               ];
+              LD_LIBRARY_PATH = "/run/opengl-driver/lib:/run/opengl-driver/32/lib";
             };
           }
           // {default = config.packages.lively;};
